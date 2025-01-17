67428101712446e164dde2d59f2b9298c23142e8
mugiwarq <95218153+mugiwarq@users.noreply.github.com>
2025-01-17 00:31:02 -0500

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -16,7 +16,7 @@ const menuitems = [
     <header class="flex flex-row flex-wrap items-center justify-between my-4 gap-x-6 gap-y-4">
       <div class="flex items-center justify-between">
         <a href="/" class="text-2xl"
-          ><span class="font-bold text-zinc-800"><span class="text-green-500">&#10070;</span> Nuxt Starter</span>
+          ><span class="font-bold text-zinc-800"><span class="text-green-500">&#10070;</span>BlockClips</span>
         </a>
       </div>
       <nav class="flex w-auto">
